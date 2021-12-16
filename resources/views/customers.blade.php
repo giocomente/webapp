a450383070a7efd8fb93ae9db9d93e25acaf92fb
Alexandr Shulmin <aleshulmin@yandex.ru>
2021-12-16 16:25:56 +0400

    init 16_12_2021_16_23

---
+++ resources/views/customers.blade.php
@@ -0,0 +1,11 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Клиенты</title>
+</head>
+<body>
+    <h1>Список клиентов.</h1>
+</body>
+</html>
